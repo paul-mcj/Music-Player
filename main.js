68d536c95b48447d82d6095800525bc3d96d47c5
paul-mcj <83841390+paul-mcj@users.noreply.github.com>
2021-08-30 14:22:12 -0400

    Add files via upload
    HTML
    - changed the source JS file to use "defer", and placed it in the head as opposed to the body
    - added input range slider for volume control:
        - set a default "value" to 50/100 when page first starts
        - set a "step" value of 5 so the input bar can scroll quicker
    - added a span element in the footer to track semantic versioning (counted each previous commit as a patch update, since this is the first time versioning is being added to the project its currently v1.0.7)
    
    CSS
    - the pauseButton and volumeControl elements are now "display:none" by default
    - the class .volume-control now shares class properties with all .button elements for streamlined styling (as well as appropriate media queries)
    - added styling for the .volume-control class
    - fixed minor styling for the new span element in the footer
    
    
    JS
    - added a querySelector for the new volumeControl slider
    - placed a default "display:none" for the buttonPause to CSS and deleted from global scope in JS file
    - added an event listener for the volumeControl upon any input changes:
        - the listener calls a new function setVolume() which...
    - added an event listener for the window object upon resizing. Since the new volumeControl only shows up on non-mobile displays, there needed to be a way to check that it would only show up for screen-widths above 40em.
    - split the playAndPause() function into two functions. Now playing and pausing songs isn't reliant on an event that targets through large if/else statements
    - drastically simplified the playlistLogic() function; the music player operates at a smaller level now, with functions only dealing with unique changes as opposed to everything occurring in one massive function:
        - the previous button its its own function, and simply absorbs the old logic of the checkBeginning() function
        - the next button its its own function, and simply absorbs the old logic of the checkEnd() function
        - repeat button does the same, simply repeating the current song from the beginning
        - the shufflePlaylist() function remains the same as its recursive, but the new shuffleTrack() function takes its value in to shuffle the songs
        - all of the newest separated functions (the four above) all also now invoke two new functions:
            - playlistLogic(), which pauses the currentSong and resets its current time to 0
            - and updateTrack(), which occurs after each uniquely called function to update the currentTrack number for the display.
    - the showButtonAndTrackNumber() isn't needed anymore with the current additions above
    - the setVolume() function grabs the value of the volumeControl element and sets the html audio object to that volume

--- main.js
+++ main.js
@@ -5,22 +5,20 @@ const buttonNext = document.querySelector(".button--next");
 const buttonPrev = document.querySelector(".button--prev");
 const buttonShuffle = document.querySelector(".button--shuffle");
 const buttonRepeat = document.querySelector(".button--repeat");
+const volumeControl = document.querySelector(".volume-control");
 const track = document.querySelector(".track-number");
 const trackName = document.querySelector(".track-name");
+const headersWithinTrackName = document.querySelectorAll(".track-name > h3");
 const trackInfo = document.querySelector(".track-info");
 const trackTimeStart = document.querySelector(".track-time__start");
 const trackTimeEnd = document.querySelector(".track-time__end");
 const progress = document.querySelector(".progress");
-const withinTrackName = document.querySelectorAll(".track-name > h3");
 
-// ||| Initialize local variables
+// ||| Initialize local variables -- some of these can be changed depending on the number of tracks to be added to the playlist over time
 const playlistLength = 21;
 let playlist = [];
 let currentTrack = 0;
-
-// ||| Initialize HTML elements
 track.textContent = 1;
-buttonPause.style.display = "none";
 
 // ||| Build the array of audio tracks
 for (i = 1; i <= playlistLength + 1; i++) {
@@ -34,71 +32,107 @@ loadEventListeners();
 
 // ||| Loads all event listeners
 function loadEventListeners() {
-    buttonPlay.addEventListener("click", playAndPause);
-    buttonPause.addEventListener("click", playAndPause);
-    buttonNext.addEventListener("click", playlistLogic);
-    buttonPrev.addEventListener("click", playlistLogic);
-    buttonShuffle.addEventListener("click", playlistLogic);
-    buttonRepeat.addEventListener("click", playlistLogic);
+    buttonPlay.addEventListener("click", playTrack);
+    buttonPause.addEventListener("click", pauseTrack);
+    buttonNext.addEventListener("click", nextTrack);
+    buttonPrev.addEventListener("click", prevTrack);
+    buttonShuffle.addEventListener("click", shuffleTrack);
+    buttonRepeat.addEventListener("click", repeatTrack);
+    volumeControl.addEventListener("input", setVolume);
+    window.addEventListener("resize", showVolumeControl);
 }
 
-// ||| Determines if the event plays or pauses the current song and sets the button accordingly
-function playAndPause(event) {
-    if (
-        event.target.classList.contains("button--play") ||
-        event.target.parentElement.classList.contains("button--play")
-    ) {
-        playlist[currentTrack].play();
-        showPauseButtonAndTrackNumber();
-        // trackTime(); fixme: don't reinstate until progress bar works for all songs
-    } else {
-        playlist[currentTrack].pause();
-        buttonPause.replaceWith(buttonPlay);
-        buttonPlay.style.display = "block";
-    }
+// ||| Plays the currentTrack and replaces the play button with the pause button. Sets the volume for the currentTrack, and will also display all other buttons and the volume control.
+function playTrack() {
+    playlist[currentTrack].play();
+    setVolume();
+    buttonPlay.replaceWith(buttonPause);
+    buttonPause.style.display = "block";
     showHidden();
+    showVolumeControl();
+    //todo: include songProgress(); once input range is working properly
 }
 
-// ||| Makes it so that every song other than the current one is not playing and will restart from the beginning if it is played again. It then checks the next song to be played (depending on if it was "next" or "previous" in the playlist), sets the counter and makes that the new current song to play.
-function playlistLogic(event) {
+// ||| Pauses the currentTrack and replaces the pause button with the play button
+function pauseTrack() {
+    playlist[currentTrack].pause();
+    buttonPause.replaceWith(buttonPlay);
+    buttonPlay.style.display = "block";
+}
+
+// ||| Checks if the current song is at the end of the playlist. If it is then the next song cycles back to the beginning, otherwise its simply the next song in the array.
+function nextTrack() {
+    playlistLogic();
+    if (currentTrack < playlistLength) {
+        currentTrack++;
+    } else {
+        currentTrack = 0;
+    }
+    updateTrack();
+}
+
+// ||| Checks if the current song is at the beginning of the playlist, if it is then the next song played (ie. the "previous" one in the playlist) cycles back to the end of the playlist.
+function prevTrack() {
+    playlistLogic();
+    if (currentTrack > 0) {
+        currentTrack--;
+    } else if (currentTrack <= 0) {
+        currentTrack = playlistLength;
+    }
+    updateTrack();
+}
+
+// ||| Calls the recursive function shufflePlaylist to find a new unique track to set as the newest currentTrack and play it.
+function shuffleTrack() {
+    playlistLogic();
+    shufflePlaylist();
+    updateTrack();
+}
+
+// ||| Repeats the current song in the playlist
+function repeatTrack() {
+    playlistLogic();
+    playlist[currentTrack].play();
+    updateTrack();
+}
+
+// ||| Updates the currentTrack number for the user and also calls the playTrack() function to have the song start playing (used in conjunction with other functions)
+function updateTrack() {
+    track.textContent = currentTrack + 1;
+    playTrack();
+}
+
+// ||| Pauses the currentTrack and resets it currentTime back to the beginning (used in conjunction with other functions)
+function playlistLogic() {
     playlist[currentTrack].pause();
     playlist[currentTrack].currentTime = 0;
-    if (
-        event.target.classList.contains("button--prev") ||
-        event.target.parentElement.classList.contains("button--prev")
-    ) {
-        checkBeginning();
-    } else if (
-        event.target.classList.contains("button--next") ||
-        event.target.parentElement.classList.contains("button--next")
-    ) {
-        checkEnd();
-    } else if (
-        event.target.classList.contains("button--shuffle") ||
-        event.target.parentElement.classList.contains("button--shuffle")
-    ) {
-        shufflePlaylist();
-    } else if (
-        event.target.classList.contains("button--repeat") ||
-        event.target.parentElement.classList.contains("button--repeat")
-    ) {
-        repeatSong();
-    }
-    track.textContent = currentTrack + 1;
-    playlist[currentTrack].play();
-    showPauseButtonAndTrackNumber();
-    showHidden();
-    // trackTime(); fixme: don't reinstate until progress bar works for all songs
 }
 
-// ||| The first time a user clicks on a valid button to start the music player app, any elements of the original html with a display "hidden" are shown.
+// ||| The first time a user clicks on the play button to start the music player app, all other buttons of the original html with "display:none" are then changed to be shown.
 function showHidden() {
+    // fixme: add transitions??
     trackName.style.display = "flex";
     trackInfo.style.display = "flex";
     buttonRepeat.style.display = "block";
     buttonShuffle.style.display = "block";
     buttonPrev.style.display = "block";
     buttonNext.style.display = "block";
+}
+
+// ||| Makes sure that the volume control only shows on non-mobile displays.
+function showVolumeControl() {
+    // The number 640 is used as the limiter because it is 40em divided by 16 in pixels. 40em is the breakpoint in the CSS file from mobile to larger displays, so its used here for consistency, and 16 is the default calculated value used in the CSS, too as determined by the global font-size.
+    // Check a random button's current display before showing the volume control element -- if it is still not showing, neither should volume control. But if the button is showing, then the innerWidth of the page must not be mobile device for the control to show, too.
+    if (window.innerWidth > 639 && buttonShuffle.style.display === "block") {
+        volumeControl.style.display = "block";
+    } else {
+        volumeControl.style.display = "none";
+    }
+}
+
+// ||| Gathers the value of the volumeControl element and sets the current track's volume to that number
+function setVolume() {
+    playlist[currentTrack].volume = volumeControl.value / 100;
 }
 
 // ||| Finds a random number between 0 and the playlistLength (inclusive) to return as the next currentTrack. If the number returned is the same as the currentTrack, the function is recalled recursively until a unique random track is returned.
@@ -115,84 +149,52 @@ function shufflePlaylist() {
     }
 }
 
-// ||| Repeats the current song in the playlist
-function repeatSong() {
-    playlist[currentTrack].pause();
-    playlist[currentTrack].currentTime = 0;
-    playlist[currentTrack].play();
-}
+// function songProgress() {
+//     let seconds = 0;
+//     let minutes = 0;
+//     let mins = Math.floor(playlist[currentTrack].duration / 60);
+//     let secs = Math.floor(playlist[currentTrack].duration % 60);
+//     let progressValue = playlist[currentTrack].duration / 100;
+//     let progressMax = playlist[currentTrack].duration;
+//     trackTimeStart.textContent = "0:00";
+//     if (secs >= 10) {
+//         trackTimeEnd.textContent = `${mins}:${secs}`;
+//     } else {
+//         trackTimeEnd.textContent = `${mins}:0${secs}`;
+//     }
 
-// ||| Make sure that every time a new song is played, the default function of the middle button (ie. the play and pause button) is always to pause the current track by hiding the play button -- the only time the pause icon is displayed is if the user clicks the play button or when the page reloads.
-function showPauseButtonAndTrackNumber() {
-    buttonPlay.replaceWith(buttonPause);
-    buttonPause.style.display = "block";
-    withinTrackName.forEach(function (track) {
-        track.style.display = "inline";
-    });
-}
+//     progress.setAttribute("max", progressMax);
 
-// ||| Checks if the current song is at the end of the playlist, if it is then the next song cycles back to the beginning of the playlist.
-function checkEnd() {
-    if (currentTrack < playlistLength) {
-        currentTrack++;
-    } else {
-        currentTrack = 0;
-    }
-    return currentTrack;
-}
+//     setInterval(countingSeconds, 1000);
+//     //todo: add automatic next song when the duration of a song is reached -- the trackTimeStart and progress bar cannot go past the duration of a song!!!
+//     //fixme: restart this function to 0 once a new current song is playing (and pause the numbers when pause is evoked)
+//     function countingSeconds() {
+//         seconds++;
+//         //todo: switch//case statements
+//         if (seconds < 10) {
+//             trackTimeStart.textContent = `${minutes}:0${seconds}`;
+//         }
+//         if (seconds > 9 && seconds < 60) {
+//             trackTimeStart.textContent = `${minutes}:${seconds}`;
+//         } else if (seconds > 59) {
+//             seconds = 0;
+//             minutes++;
+//             trackTimeStart.textContent = `${minutes}:0${seconds}`;
+//         }
+//         progressValue++;
+//         progress.setAttribute("value", progressValue);
+//         console.log(`track = ${currentTrack}    value = ${progressValue}   max = ${progressMax}`);
+//         if (progressValue === progressMax || progressValue >= progressMax) {
+//             console.log("next song");
+//             nextTrack();
+//         }
+//     }
+// }
 
-// ||| Checks if the current song is at the beginning of the playlist, if it is then the next song played (ie. the "previous" one in the playlist) cycles back to the end of the playlist.
-function checkBeginning() {
-    if (currentTrack > 0) {
-        currentTrack--;
-    } else if (currentTrack <= 0) {
-        currentTrack = playlistLength;
-    }
-    return currentTrack;
-}
-
-function trackTime() {
-    let seconds = 0;
-    let minutes = 0;
-    let mins = Math.floor(playlist[currentTrack].duration / 60);
-    let secs = Math.floor(playlist[currentTrack].duration % 60);
-    let progressValue = playlist[currentTrack].duration / 100;
-    let progressMax = playlist[currentTrack].duration;
-    trackTimeStart.textContent = "0:00";
-    if (secs >= 10) {
-        trackTimeEnd.textContent = `${mins}:${secs}`;
-    } else {
-        trackTimeEnd.textContent = `${mins}:0${secs}`;
-    }
-
-    progress.setAttribute("max", progressMax);
-
-    setInterval(countingSeconds, 1000);
-    //todo: add automatic next song when the duration of a song is reached -- the trackTimeStart and progress bar cannot go past the duration of a song!!!
-    //fixme: restart this function to 0 once a new current song is playing (and pause the numbers when pause is evoked)
-    function countingSeconds() {
-        seconds++;
-        //todo: switch//case statements
-        if (seconds < 10) {
-            trackTimeStart.textContent = `${minutes}:0${seconds}`;
-        }
-        if (seconds > 9 && seconds < 60) {
-            trackTimeStart.textContent = `${minutes}:${seconds}`;
-        } else if (seconds > 59) {
-            seconds = 0;
-            minutes++;
-            trackTimeStart.textContent = `${minutes}:0${seconds}`;
-        }
-        progressValue++;
-        progress.setAttribute("value", progressValue);
-        console.log(`track = ${currentTrack}    value = ${progressValue}   max = ${progressMax}`);
-    }
-}
-
+//fixme: fix event delegation for volume slider (it should be scrollable while hovering, not just after clicking it)
 //todo: add repeat function: make it repeat after the current song is done playing (need to check if current song is still playing?). change from current logic of simply restarting the song from the beginning
 //todo: make progress bar transition smoother?
 //todo: add slider functionality to have users skip through songs
-//todo: add volume control
 //todo: check audio files have loaded in before starting (NaN is begin returned prematurely for the tractTime() function) -- add an alert that music cannot be played when files are loading
 //todo: persist storage?
 //todo: drag and drop for playlists eventually
